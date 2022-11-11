4f683060b8be04bab85e15ee2b17a1d46b0dd52e
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-11 19:57:43 +0700

    add public bool ListEmpty()

--- singlylinkedlist/Program.cs
+++ singlylinkedlist/Program.cs
@@ -107,6 +107,12 @@ namespace singly_linked_list
             else
                 return (true);
         }
-
+        public bool ListEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
     }
 }
