3188b55de24f6373f7432200852b559b2dec5885
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-11 17:27:57 +0700

    add if (START == null || nim <= START.rollNumber)

--- singlylinkedlist/Program.cs
+++ singlylinkedlist/Program.cs
@@ -29,7 +29,17 @@ namespace singly_linked_list
             newnode.rollNumber = nim;
             newnode.name = nm;
             //if the node to be inserted is the first node
-
+            if (START == null || nim <= START.rollNumber)
+            {
+                if ((START != null) && (nim == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed\n");
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
         }
     }
 }
