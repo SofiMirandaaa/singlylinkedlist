d5e76a4146895020570e483b63d8f4ea5d1aa845
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-11 17:22:42 +0700

    add node in the list

--- singlylinkedlist/Program.cs
+++ singlylinkedlist/Program.cs
@@ -19,6 +19,16 @@ namespace singly_linked_list
         }
         public void  addNote() //add node in the list
         {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number o the student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            //if the node to be inserted is the first node
 
         }
     }
